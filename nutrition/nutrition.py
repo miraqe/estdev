671d1368456581e546bcf3b1b754d550adb9854d
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 15:36:58 +0000

    Mon, Apr 3, 2023, 6:36 PM +03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,39 +1,34 @@
-# Create a dictionary of fruits and their calories per portion
-fruit_calories = {
+# Create a dictionary to store the fruits and their calorie information
+fruits = {
     "Apple": 130,
-    "Apricot": 17,
     "Avocado": 50,
-    "Banana": 105,
-    "Blackberry": 62,
-    "Blueberry": 57,
-    "Cantaloupe": 60,
-    "Cherry": 50,
-    "Clementine": 35,
-    "Coconut meat": 283,
-    "Grapes": 52,
-    "Guava": 37,
-    "Honeydew melon": 64,
-    "Kiwi": 46,
+    "Blackberries": 60,
+    "Blueberries": 80,
+    "Cantaloupe": 50,
+    "Cherries": 90,
+    "Grapefruit": 60,
+    "Grapes": 60,
+    "Honeydew Melon": 50,
+    "Kiwi": 90,
     "Mango": 135,
-    "Nectarine": 70,
-    "Orange": 70,
-    "Papaya": 120,
-    "Peach": 59,
+    "Nectarine": 60,
+    "Orange": 80,
+    "Papaya": 60,
+    "Peach": 60,
     "Pear": 100,
-    "Pineapple": 82,
-    "Plum": 46,
-    "Raspberries": 64,
-    "Strawberries": 49,
-    "Sweet Cherries": 100,
-    "Watermelon": 46
+    "Pineapple": 80,
+    "Plums": 70,
+    "Raspberries": 60,
+    "Strawberries": 50
 }
 
-# Take input from the user
-fruit = input("Enter a fruit: ")
+# Get the user input and convert it to lowercase
+fruit = input().lower()
 
-# Convert the input to lowercase for case-insensitivity
-fruit = fruit.lower()
-
-# Check if the input fruit is in the dictionary and print its calories if it is
-if fruit in fruit_calories:
-    print("Calories: ", fruit_calories[fruit])
+# Check if the user input matches a fruit in the dictionary
+if fruit in fruits:
+    # If the fruit is in the dictionary, print its calorie information
+    print("Calories:", fruits[fruit])
+else:
+    # If the fruit is not in the dictionary, print nothing
+    pass
