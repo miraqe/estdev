e2b8ce0a19146745f8d4ed55448299fda9831cf2
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 10:57:15 +0000

    Mon, Apr 3, 2023, 1:57 PM +03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -2,11 +2,20 @@
 expression = input("Enter an arithmetic expression (e.g. 1 + 1): ")
 
 # Split the input into its constituent parts
-x, operator, y = expression.split()
+parts = expression.split()
+if len(parts) != 3:
+    print("Error: Invalid input")
+    exit()
+
+x, operator, y = parts
 
 # Convert x and y to floats
-x = float(x)
-y = float(y)
+try:
+    x = float(x)
+    y = float(y)
+except ValueError:
+    print("Error: Invalid input")
+    exit()
 
 # Perform the appropriate arithmetic operation
 if operator == "+":
@@ -17,6 +26,9 @@ elif operator == "*":
     result = x * y
 elif operator == "/":
     result = x / y
+else:
+    print("Error: Invalid operator")
+    exit()
 
 # Output the result as a floating-point value formatted to one decimal place
 print("{:.1f}".format(result))
