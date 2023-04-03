ec81674cd349c7fd53b2dad2b7c05220b48d0410
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 11:22:56 +0000

    Mon, Apr 3, 2023, 2:22 PM +03:00

---
+++ coke.py
@@ -0,0 +1,35 @@
+# Define the price of a bottle of Coke
+PRICE = 50
+
+# Define the valid denominations
+DENOMINATIONS = [25, 10, 5]
+
+# Initialize the total amount inserted by the user
+total = 0
+
+# Loop until the user has inserted enough money
+while total < PRICE:
+    # Prompt the user to insert a coin
+    coin = int(input("Insert a coin (25, 10, or 5): "))
+
+    # Check if the coin is a valid denomination
+    if coin in DENOMINATIONS:
+        # Add the coin to the total
+        total += coin
+
+        # Inform the user of the amount due
+        due = PRICE - total
+        if due > 0:
+            print(f"Amount due: {due} cents")
+    else:
+        # Ignore invalid coins
+        print("Invalid coin. Please insert a valid denomination.")
+
+# Calculate the change owed to the user
+change = total - PRICE
+
+# Output the change owed to the user
+if change > 0:
+    print(f"Change owed: {change} cents")
+else:
+    print("Thank you for your purchase!")
