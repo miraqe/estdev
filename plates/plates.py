9127a59660d74adf091d8781f4525045b148a6e7
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 14:16:22 +0000

    Mon, Apr 3, 2023, 5:16 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,23 +1,13 @@
 def is_vanity_plate(s):
-    # Check length
+    # Check length of the string
     if len(s) < 2 or len(s) > 6:
         return False
-
     # Check that first two characters are letters
     if not s[:2].isalpha():
         return False
-
-    # Check that last characters are digits
-    if not s[-4:].isdigit():
+    # Check that the last characters are digits and not 0
+    if not s[2:].isdigit() or s[-1] == '0':
         return False
-
-    # Check that first character of number sequence is not '0'
-    index = 2
-    while index < len(s) and s[index].isalpha():
-        index += 1
-    if index < len(s) and s[index] == '0':
-        return False
-
     return True
 
 
