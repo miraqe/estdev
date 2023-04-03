b7d74a732991a48cf7a528c270f03b783817f03d
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 14:41:41 +0000

    Mon, Apr 3, 2023, 5:41 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,22 +1,35 @@
 def is_vanity_plate(s):
-    if not s.isalnum():
-        return False
+    # check that plate has at least 2 and at most 6 characters
+    if not 2 <= len(plate) <= 6:
+        return "Invalid"
 
-    if len(s) < 3 or len(s) > 6:
-        return False
+    # check that first 2 characters are letters
+    if not plate[:2].isalpha():
+        return "Invalid"
 
-    if not s[:2].isalpha():
-        return False
+    # check that the last characters are digits
+    if not plate[-4:].isdigit():
+        return "Invalid"
 
-    num_count = 0
-    for i in range(2, len(s)):
-        if s[i].isdigit():
-            num_count += 1
-            if num_count > 4:
-                return False
-        elif not s[i].isalpha():
-            return False
-    return True
+    # check that there are no letters after the numbers
+    if plate[-3:].isalpha():
+        return "Invalid"
+
+    # check that the first digit is not 0 if it's in position 3
+    if len(plate) >= 4 and plate[2].isdigit() and plate[2] == '0':
+        return "Invalid"
+
+    # check that the first digit is not 0 if it's in position 4 or 5
+    if len(plate) >= 5 and plate[3].isdigit() and plate[3] == '0':
+        return "Invalid"
+
+    # check that there are no numbers in between letters
+    for i in range(2, len(plate)-4):
+        if plate[i].isdigit() or plate[i+1].isdigit():
+            return "Invalid"
+
+    # if all checks pass, return "Valid"
+    return "Valid"
 
 
 
