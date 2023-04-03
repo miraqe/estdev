c4314033f76a2d8b0a7832b633c0c1d3d19d3378
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 13:28:36 +0000

    Mon, Apr 3, 2023, 4:28 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,8 +1,34 @@
 def is_valid(s):
+    # Check length of string
     if len(s) < 2 or len(s) > 6:
         return False
-    if not s[:-1].isalpha() or not s[-1].isdigit():
+
+    # Check that first two characters are letters
+    if not s[:2].isalpha():
         return False
+
+    # Check that remaining characters are either letters or digits
+    if not s[2:].isalnum():
+        return False
+
+    # Check that last character is a letter (not a digit)
+    if not s[-1].isalpha():
+        return False
+
+    # Check that first character is not 0
     if s[0] == '0':
         return False
+
+    # All checks passed, so string is valid
     return True
+
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+main()
