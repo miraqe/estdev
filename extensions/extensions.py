7ba8283e75440560e1edb3d1cf41e2f7e59f073f
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 10:52:12 +0000

    Mon, Apr 3, 2023, 1:52 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,7 +1,7 @@
 def get_media_type(file_name):
     """Given a file name, returns the corresponding media type based on the file's extension."""
     extensions = {'.gif': 'image/gif', '.jpg': 'image/jpeg', '.jpeg': 'image/jpeg', '.png': 'image/png', '.pdf': 'application/pdf', '.txt': 'text/plain', '.zip': 'application/zip'}
-    file_extension = '.' + file_name.split('.')[-1].lower()  # Get the file extension (if any), convert to lowercase, and add a period
+    file_extension = '.' + file_name.strip().split('.')[-1].lower()  # Get the file extension (if any), convert to lowercase, and add a period
     if file_extension in extensions:
         return extensions[file_extension]
     else:
