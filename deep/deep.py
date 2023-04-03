b0b9f832f1ec1d6c6ec0697a146a70e99497c50f
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 10:08:57 +0000

    Mon, Apr 3, 2023, 1:08 PM +03:00

---
+++ deep/deep.py
@@ -0,0 +1,9 @@
+def main():
+    answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+    if answer.lower() == "42" or answer.lower() == "forty-two" or answer.lower() == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == "__main__":
+    main()
