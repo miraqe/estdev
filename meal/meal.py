2768ce510a4f2d891bc3d3d3854d893c9c264797
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-03 10:24:19 +0000

    Mon, Apr 3, 2023, 1:24 PM +03:00

---
+++ meal/meal.py
@@ -0,0 +1,17 @@
+def convert(time):
+    hours, minutes = time.split(':')
+    return float(hours) + float(minutes) / 60
+
+def main():
+    time = input('Enter the time (in 24-hour format, as ##:##): ')
+    hour = convert(time)
+
+    if 7 <= hour < 8:
+        print('It\'s breakfast time!')
+    elif 12 <= hour < 13:
+        print('It\'s lunch time!')
+    elif 18 <= hour < 19:
+        print('It\'s dinner time!')
+
+if __name__ == '__main__':
+    main()
