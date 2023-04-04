b0bbbd003b698c6e989e84d4dbf4d009c9357149
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-04 09:50:32 +0000

    Tue, Apr 4, 2023, 12:50 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,38 +1,32 @@
-import re
+from datetime import datetime
 
-pattern = r"^(0?[1-9]|1[0-2])([-\/\s])(0?[1-9]|[12]\d|3[01])\2(\d{4})$|^(January|February|March|April|May|June|July|August|September|October|November|December)\s(0?[1-9]|[12]\d|3[01]),\s(\d{4})$"
+MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
 while True:
-    date = input("Enter a date (month/day/year or month day, year): ")
-    match = re.match(pattern, date)
-    if match:
-        groups = match.groups()
-        if groups[0].isdigit():
-            month = int(groups[0])
-            day = int(groups[2])
-            year = int(groups[3])
-        else:
-            month = [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December"
-            ].index(groups[4]) + 1
-            day = int(groups[5])
-            year = int(groups[6])
-        try:
-            new_date = "{:04d}-{:02d}-{:02d}".format(year, month, day)
-            print(new_date)
-            break
-        except ValueError:
-            print("Invalid date format. Please try again.")
-    else:
+    try:
+        date_str = input("Enter a date in the format MM/DD/YYYY or Month Day, Year: ")
+        date_str = date_str.replace(",", "")
+        parts = date_str.split()
+        if len(parts) == 3:
+            month, day, year = parts
+            month_num = MONTHS.index(month) + 1
+            date_str = f"{month_num:02}/{day}/{year}"
+        datetime_obj = datetime.strptime(date_str, "%m/%d/%Y")
+        break
+    except ValueError:
         print("Invalid date format. Please try again.")
+
+print(datetime_obj.strftime("%Y-%m-%d"))
