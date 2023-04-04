7fb5476bcff700ee5a9380418d552f4fb2141de0
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-04 10:03:35 +0000

    Tue, Apr 4, 2023, 1:03 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,18 +17,24 @@ MONTHS = [
 
 while True:
     try:
-        date_str = input("Enter a date in the format MM/DD/YYYY or Month Day, Year: ")
+        date_str = input("Enter a date in the format MM/DD/YYYY or Month Day, Year: ").strip()
         date_str = date_str.replace(",", "")
         parts = date_str.split()
+
+        # Case when user input is Month Day, Year format
         if len(parts) == 3:
             month, day, year = parts
-            month_num = MONTHS.index(month.title()) + 1
+            if month not in MONTHS:
+                raise ValueError("Invalid date format")
+            month_num = MONTHS.index(month) + 1
             date_str = f"{month_num:02}/{day}/{year}"
+
+        # Case when user input is MM/DD/YYYY format
         datetime_obj = datetime.strptime(date_str, "%m/%d/%Y")
         break
+
     except ValueError:
-        if not date_str.strip():
-            continue
         print("Invalid date format. Please try again.")
+        continue
 
 print(datetime_obj.strftime("%Y-%m-%d"))
