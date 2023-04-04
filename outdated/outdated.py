67aa8b686eee92ccf6256f2c0e7d988d77f5c914
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-04 09:39:51 +0000

    Tue, Apr 4, 2023, 12:39 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,6 +1,5 @@
-import datetime
+from datetime import datetime
 
-# List of month names in order
 MONTHS = [
     "January",
     "February",
@@ -18,20 +17,16 @@ MONTHS = [
 
 while True:
     try:
-        # Prompt the user for a date in month-day-year format
-        date_str = input("Enter a date in month-day-year format (e.g., 9/8/1636): ")
-        # Try to parse the date using strptime
-        date_obj = datetime.datetime.strptime(date_str, "%m/%d/%Y")
+        date_str = input("Enter a date in the format MM/DD/YYYY or Month Day, Year: ")
+        date_str = date_str.replace(",", "")
+        parts = date_str.split()
+        if len(parts) == 3:
+            month, day, year = parts
+            month_num = MONTHS.index(month) + 1
+            date_str = f"{month_num:02}/{day}/{year}"
+        datetime_obj = datetime.strptime(date_str, "%m/%d/%Y")
+        break
     except ValueError:
-        try:
-            # If parsing fails, try to parse the date in month name-day-year format
-            month_name, day_str, year_str = date_str.split()
-            month_num = MONTHS.index(month_name) + 1
-            date_obj = datetime.datetime.strptime(f"{month_num}/{day_str}/{year_str}", "%m/%d/%Y")
-        except (ValueError, IndexError):
-            # If parsing fails again, prompt the user again
-            print("Invalid date format. Please try again.")
-            continue
-    # Format the date in year-month-day format and print it
-    print(date_obj.strftime("%Y-%m-%d"))
-    break
+        print("Invalid date format. Please try again.")
+
+print(datetime_obj.strftime("%Y-%m-%d"))
