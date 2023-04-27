0d10046a53c2e94990b1b9740f3f6cae8094e1cd
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-27 12:28:01 +0000

    Thu, Apr 27, 2023, 3:28 PM +03:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,11 +1,9 @@
 import emoji
 
-emoji_dict = emoji.EMOJI_ALIAS_UNICODE
 
 text = input("Enter a string: ")
+output = emoji.emojize(text)
 
-for alias, emoji_char in emoji_dict.items():
-    # Replace the alias with the corresponding emoji
-    text = text.replace(alias, emoji_char)
 
-print(text)
+
+print("Output: ", text)
