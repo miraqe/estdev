2a8ac0889d864a5945fcbf548d7b793854034966
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-27 12:35:28 +0000

    Thu, Apr 27, 2023, 3:35 PM +03:00

---
+++ figlet/figlet.py
@@ -0,0 +1,26 @@
+import sys
+import random
+from pyfiglet import Figlet
+
+def print_usage():
+    print("Usage: figlet.py [-f FONT] [TEXT]")
+    sys.exit(1)
+
+# Parse command-line arguments
+if len(sys.argv) == 1:
+    font_name = random.choice(Figlet().getFonts())
+    text = input("Enter text: ")
+elif len(sys.argv) == 3 and sys.argv[1] in ("-f", "--font"):
+    font_name = sys.argv[2]
+    text = input("Enter text: ")
+else:
+    print_usage()
+
+# Check if font is valid
+if font_name not in Figlet().getFonts():
+    print(f"Error: invalid font '{font_name}'")
+    sys.exit(1)
+
+# Print text in chosen font
+figlet = Figlet(font=font_name)
+print(figlet.renderText(text))
