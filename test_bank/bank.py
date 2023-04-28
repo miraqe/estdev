a680898f3090e25f08b09cb75751c9d170817372
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-28 11:16:24 +0000

    Fri, Apr 28, 2023, 2:16 PM +03:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -5,8 +5,8 @@ def main():
 
 def value(greeting):
     greeting = greeting.lower().strip()
-    if "hello" in greeting:
-        return 0
+    """if "hello" in greeting:
+        return 0"""
     elif "h" == greeting[0]:
         return 20
     else:
