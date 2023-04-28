ccebbab7b8bf67bfc2c63308470ad3c38e1a2ad4
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-28 11:52:35 +0000

    Fri, Apr 28, 2023, 2:52 PM +03:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,31 @@
+def get_fraction():
+    while True:
+        fraction = input("Enter a fraction in the format X/Y: ")
+        try:
+            x, y = map(int, fraction.split('/'))
+            if y == 0 or x > y:
+                raise ValueError
+            return x, y
+        except (ValueError, ZeroDivisionError):
+            continue
+
+def calculate_percentage(x, y):
+    percentage = (x / y) * 100
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return str(round(percentage)) + "%"
+
+if __name__ == "__main__":
+    x, y = get_fraction()
+    percentage = calculate_percentage(x, y)
+    if percentage[-1] == "%":
+        percentage = percentage[:-1]
+    if percentage == "E":
+        print("E")
+    elif percentage == "F":
+        print("F")
+    else:
+        print("{}%".format(percentage))
