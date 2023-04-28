27e202cc62f50008b369889e634d186dea19132e
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-28 11:07:23 +0000

    Fri, Apr 28, 2023, 2:07 PM +03:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,12 +1,10 @@
 def main():
     greeting = input("Greeting: ")
     value_to_print = value(greeting)
-    if greeting.startswith("hello"):
     print(f"${value_to_print}")
 
 def value(greeting):
     greeting = greeting.lower().strip()
-
     if "hello" in greeting:
         return 0
     elif "h" == greeting[0]
