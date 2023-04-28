be7bbf6f65729a6eec6ff89bf329e00b6a2e08a2
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-28 11:26:17 +0000

    Fri, Apr 28, 2023, 2:26 PM +03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,4 +1,4 @@
-from plates import is_valid
+from plates import is_vanity_plate
 
 def main():
     test_start()
@@ -8,42 +8,42 @@ def main():
     test_punc()
 
 def test_start():
-    assert is_valid('ABCDEF') == True
-    assert is_valid('1ABCDE') == False
-    assert is_valid('A3CDEF') == False
-    assert is_valid('63CDEF') == False
+    assert is_vanity_plate('ABCDEF') == True
+    assert is_vanity_plate('1ABCDE') == False
+    assert is_vanity_plate('A3CDEF') == False
+    assert is_vanity_plate('63CDEF') == False
 
 def test_length():
-    assert is_valid('ABCDEF') == True
-    assert is_valid('ABCDE') == True
-    assert is_valid('ABC') == True
-    assert is_valid('AB') == True
-    assert is_valid('A') == False
+    assert is_vanity_plate('ABCDEF') == True
+    assert is_vanity_plate('ABCDE') == True
+    assert is_vanity_plate('ABC') == True
+    assert is_vanity_plate('AB') == True
+    assert is_vanity_plate('A') == False
 
 def test_numbers():
-    assert is_valid('ABC123') == True
-    assert is_valid('ABCD12') == True
-    assert is_valid('ABCDE1') == True
-    assert is_valid('AB23EF') == False
-    assert is_valid('ABC23F') == False
-    assert is_valid("AA") == True
-    assert is_valid("A2") == False
-    assert is_valid("2A") == False
-    assert is_valid("22") == False
-    assert is_valid(" 2") == False
+    assert is_vanity_plate('ABC123') == True
+    assert is_vanity_plate('ABCD12') == True
+    assert is_vanity_plate('ABCDE1') == True
+    assert is_vanity_plate('AB23EF') == False
+    assert is_vanity_plate('ABC23F') == False
+    assert is_vanity_plate("AA") == True
+    assert is_vanity_plate("A2") == False
+    assert is_vanity_plate("2A") == False
+    assert is_vanity_plate("22") == False
+    assert is_vanity_plate(" 2") == False
 
 def test_zero():
-    assert is_valid('ABC102') == True
-    assert is_valid('CS50') == True
-    assert is_valid('ABC012') == False
-    assert is_valid('ABCD01') == False
+    assert is_vanity_plate('ABC102') == True
+    assert is_vanity_plate('CS50') == True
+    assert is_vanity_plate('ABC012') == False
+    assert is_vanity_plate('ABCD01') == False
 
 def test_punc():
-    assert is_valid('ABC,23') == False
-    assert is_valid('ABC 23') == False
-    assert is_valid('ABC.12') == False
-    assert is_valid('AB:12') == False
-    assert is_valid('AB/45') == False
+    assert is_vanity_plate('ABC,23') == False
+    assert is_vanity_plate('ABC 23') == False
+    assert is_vanity_plate('ABC.12') == False
+    assert is_vanity_plate('AB:12') == False
+    assert is_vanity_plate('AB/45') == False
 
 if _name_ == "__main__":
     main()
