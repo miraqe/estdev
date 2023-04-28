3faca7a0da943c685038949dac5ea8a4818055a0
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-28 11:13:33 +0000

    Fri, Apr 28, 2023, 2:13 PM +03:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -7,7 +7,7 @@ def value(greeting):
     greeting = greeting.lower().strip()
     if "hello" in greeting:
         return 0
-    elif "h" == greeting[0]
+    elif "h" == greeting[0]:
         return 20
     else:
         return 100
