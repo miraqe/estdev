3abd77091db96fb498c3a24f9dd75ec67f9ceaac
miraqe <129060155+miraqe@users.noreply.github.com>
2023-04-28 11:10:28 +0000

    Fri, Apr 28, 2023, 2:10 PM +03:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -8,9 +8,9 @@ def value(greeting):
     if "hello" in greeting:
         return 0
     elif "h" == greeting[0]
-        return 0
+        return 20
     else:
-        return 0
+        return 100
 
 if __name__ == "__main__":
     main()
