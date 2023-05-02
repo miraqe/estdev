bf336dc6788ca7d32dbce896dd54d0faa8f12f72
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 15:48:01 +0000

    Tue, May 2, 2023, 6:48 PM +03:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,20 +1,23 @@
 import re
 import sys
 
-
 def main():
     print(parse(input("HTML: ")))
 
+def parse(html):
+    # Search for the iframe tag
+    iframe_search = re.search(r'<iframe.*?</iframe>', html, re.DOTALL)
+    if iframe_search:
+        # Extract the iframe tag
+        iframe = iframe_search.group()
 
-def parse(s):
-    match = re.search(r'src="(.*?youtube.*?)".*?</iframe>', s, re.DOTALL | re.IGNORECASE)
-    if match:
-        url = match.group(1)
-        url = re.sub(r'^//', 'https://', url)
-        return url
-    else:
-        return None
-
+        # Search for the YouTube link in the iframe tag
+        youtube_search = re.search(r'src=["\'](?P<link>https?://www\.youtube\.com/embed/(?P<id>[^/]+))', iframe)
+        if youtube_search:
+            # Extract the YouTube link and ID
+            link = youtube_search.group('link')
+            return link
+    return None
 
 if __name__ == "__main__":
     main()
