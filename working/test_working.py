1cfddfdab72600a6681fd9a15e9e326143606122
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 16:56:08 +0000

    Tue, May 2, 2023, 7:56 PM +03:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,31 +1,46 @@
+
 from working import convert
 
-def test_valid_input_1():
-    assert convert("2:30 PM to 11:45 PM") == "14:30 to 23:45"
 
+class TestWorking(unittest.TestCase):
+    def test_valid_input_1(self):
+        self.assertEqual(convert("9 AM to 5 PM"), "09:00 to 17:00")
 
-def test_valid_input_2():
-    assert convert("6:20 AM to 12:40 PM") == "06:20 to 12:40"
+    def test_valid_input_2(self):
+        self.assertEqual(convert("9:00 AM to 5:00 PM"), "09:00 to 17:00")
 
+    def test_valid_input_3(self):
+        self.assertEqual(convert("8 PM to 8 AM"), "20:00 to 08:00")
 
-def test_valid_input_3():
-    assert convert("10:00 PM to 8:00 AM") == "22:00 to 08:00"
+    def test_valid_input_4(self):
+        self.assertEqual(convert("8:00 PM to 8:00 AM"), "20:00 to 08:00")
 
+    def test_valid_input_5(self):
+        self.assertEqual(convert("12 AM to 12 PM"), "00:00 to 12:00")
 
-def test_valid_input_4():
-    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+    def test_valid_input_6(self):
+        self.assertEqual(convert("12:00 AM to 12:00 PM"), "00:00 to 12:00")
 
+    def test_invalid_input_1(self):
+        with self.assertRaises(ValueError):
+            convert("8:60 AM to 4:60 PM")
 
-def test_invalid_input_1():
-    assert convert("10:00 PM to 25:00 PM") == "Invalid input format"
+    def test_invalid_input_2(self):
+        with self.assertRaises(ValueError):
+            convert("9AM to 5PM")
 
+    def test_invalid_input_3(self):
+        with self.assertRaises(ValueError):
+            convert("09:00 to 17:00")
 
-def test_invalid_input_2():
-    assert convert("not enough values to unpack (expected 2, got 1)") == "Invalid input format"
+    def test_invalid_input_4(self):
+        with self.assertRaises(ValueError):
+            convert("9 AM - 5 PM")
 
+    def test_invalid_input_5(self):
+        with self.assertRaises(ValueError):
+            convert("10:7 AM - 5:1 PM")
 
-def test_invalid_input_3():
-    assert convert("10:00 PM to 12:00") == "Invalid input format"
-
-
-
+    def test_printing_hours_off_by_one(self):
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            convert
