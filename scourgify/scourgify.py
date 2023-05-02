b19edf40deb97d707f115b50bed99aa979d82420
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 13:03:26 +0000

    Tue, May 2, 2023, 4:03 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,7 +4,7 @@ import os.path
 
 # Check for correct number of arguments
 if len(sys.argv) != 3:
-    print("Usage: python scourgify.py before.csv after.csv")
+    print("Usage: python scourgify.py input.csv")
     sys.exit(1)
 
 # Check that input file exists
@@ -13,8 +13,8 @@ if not os.path.isfile(sys.argv[1]):
     sys.exit(1)
 
 # Check if output file exists and overwrite if necessary
-if os.path.isfile(sys.argv[2]):
-    print(f"Warning: '{sys.argv[2]}' already exists and will be overwritten.")
+if os.path.isfile("after.csv"):
+    print("Warning: 'after.csv' already exists and will be overwritten.")
 
 # Open input file and read contents
 with open(sys.argv[1], newline='') as csvfile:
@@ -23,14 +23,18 @@ with open(sys.argv[1], newline='') as csvfile:
 
 # Cleanse data
 clean_data = [[cell.strip().replace('\n', ' ') for cell in row] for row in data]
-if len(clean_data[0]) != 3 or clean_data[0][0] != 'first' or clean_data[0][1] != 'last' or clean_data[0][2] != 'house':
-    print("Error: CSV file does not have the specified format.")
+
+# Check that CSV has at least 2 rows
+if len(clean_data) < 2:
+    print("Error: CSV file must have at least 2 rows.")
     sys.exit(1)
 
+# Add header row to cleansed data
+clean_data.insert(0, ["first", "last", "house"])
+
 # Write cleansed data to output file
-with open(sys.argv[2], "w", newline='') as outfile:
+with open("after.csv", "w", newline='') as outfile:
     writer = csv.writer(outfile)
     writer.writerows(clean_data)
 
-print(f"CSV file has been cleansed and saved as '{sys.argv[2]}'.")
-
+print("CSV file has been cleansed and saved as 'after.csv'.")
