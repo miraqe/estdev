791b17c50e407fe4e363c7ea927cd7500e8bbe73
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 14:48:50 +0000

    Tue, May 2, 2023, 5:48 PM +03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,10 +1,29 @@
-import re
-
 def validate(ip):
-    bytes = ip.split('.')
+    # Split the IP address into its component bytes
+    bytes = ip.split(".")
+
+    # Check that the IP address has exactly four bytes
     if len(bytes) != 4:
         return False
+
+    # Check that each byte is an integer between 0 and 255
     for byte in bytes:
-        if not byte.isdigit() or int(byte) < 0 or int(byte) > 255:
+        try:
+            num = int(byte)
+            if num < 0 or num > 255:
+                return False
+        except ValueError:
             return False
+
+    # If we made it this far, the IP address is valid
     return True
+
+
+def main():
+    ip = input("IPv4 Address: ")
+    if not validate(ip):
+        print(f"Error: {ip} is not a valid IPv4 address")
+        sys.exit(1)
+    else:
+        print(f"{ip} is a valid IPv4 address")
+        sys.exit(0)
