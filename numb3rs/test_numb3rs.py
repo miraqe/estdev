1ec351f525c793525035ebc94c45cfc025928aff
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 15:18:43 +0000

    Tue, May 2, 2023, 6:18 PM +03:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,8 +4,11 @@ def test_valid_ipv4():
     assert validate("127.0.0.1") == True
     assert validate("192.168.0.1") == True
     assert validate("255.255.255.255") == True
+    assert validate("140.247.235.144") == True
 
 def test_invalid_ipv4():
     assert validate("512.512.512.512") == False
     assert validate("1.2.3.1000") == False
     assert validate("cat") == False
+    assert validate("256.256.256.256") == False
+    assert validate("192.168.1") == False
