149292c0488969517311d2e3b8b2dd1ad32eef0e
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 15:14:40 +0000

    Tue, May 2, 2023, 6:14 PM +03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,20 +1,19 @@
 import re
 import sys
 
+
 def main():
     print(validate(input("IPv4 Address: ")))
+    sys.exit(0)
+
 
 def validate(ip):
-    # pattern to match a valid IP address
-    pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$"
     if re.match(pattern, ip):
         return True
     else:
         return False
 
+
 if __name__ == "__main__":
-    if not validate(sys.argv[1]):
-        print("Invalid IPv4 address", file=sys.stderr)
-        sys.exit(1)
-    else:
-        sys.exit(0)
+    main()
