d5f67dd296c30ea62270f2732ace3ddc96b2c5e0
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 16:29:49 +0000

    Tue, May 2, 2023, 7:29 PM +03:00

--- working/working.py
+++ working/working.py
@@ -1,32 +1,40 @@
 import re
 
 
-def convert(time_str):
+def convert(time_string):
     # Split the time string into start and end times
-    start, end = time_str.split(" to ")
+    start_time, end_time = time_string.split(' to ')
 
     # Convert the start time to military time
-    start_hour, start_min = map(int, start[:-3].split(":"))
-    start_meridian = start[-2:]
-    if start_meridian == "PM" and start_hour != 12:
-        start_hour += 12
+    start_time = convert_to_military(start_time)
+
+    # If the end time is smaller than the start time, add 24 hours
+    if convert_to_military(end_time) < start_time:
+        end_time = add_24_hours(end_time)
 
     # Convert the end time to military time
-    end_hour, end_min = map(int, end[:-3].split(":"))
-    end_meridian = end[-2:]
-    if end_meridian == "PM" and end_hour != 12:
-        end_hour += 12
-    if end_meridian == "AM" and end_hour == 12:
-        end_hour = 0
+    end_time = convert_to_military(end_time)
 
-    # Adjust the end time if it's on the next day
-    if end_hour < start_hour:
-        end_hour += 24
+    # Convert the times to the correct format
+    start_time = format_time(start_time)
+    end_time = format_time(end_time)
 
-    # Format the start and end times in military format
-    start_time = f"{start_hour:02d}:{start_min:02d}"
-    end_time = f"{end_hour:02d}:{end_min:02d}"
+    # Combine the times into a string and return it
+    return f'{start_time} to {end_time}'
 
-    # Combine the start and end times into a string
-    return f"{start_time} to {end_time}"
+def convert_to_military(time_string):
+    # Convert the time string to military time
+    time_obj = datetime.strptime(time_string, '%I:%M %p')
+    return time_obj.hour * 100 + time_obj.minute
+
+def add_24_hours(time_string):
+    # Add 24 hours to the time string
+    time_obj = datetime.strptime(time_string, '%I:%M %p')
+    time_obj += timedelta(hours=24)
+    return time_obj.strftime('%I:%M %p')
+
+def format_time(time_value):
+    # Format the time value as a string
+    return datetime.strptime(str(time_value), '%H%M').strftime('%I:%M %p').lstrip('0')
+
 
