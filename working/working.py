c125af73ca6f9015897e6a3d9846baa1297aa04b
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 16:41:54 +0000

    Tue, May 2, 2023, 7:41 PM +03:00

--- working/working.py
+++ working/working.py
@@ -1,20 +1,20 @@
 import re
+from datetime import datetime, timedelta
 
 def convert(time_range: str) -> str:
     pattern = re.compile(r'(\d{1,2}):(\d{2})\s*([AP]M)')
-    time_1, time_2 = pattern.findall(time_range)
+    matches = pattern.findall(time_range)
 
-    # Check if the input time is valid
-    for time in (time_1, time_2):
-        hour, minute = int(time[0]), int(time[1])
-        if hour > 12 or minute >= 60:
-            return "Invalid input format"
+    if len(matches) != 2:
+        raise ValueError("Invalid input format")
 
-    # Convert to military time
-    time_1_hour, time_1_minute = int(time_1[0]), int(time_1[1])
-    time_2_hour, time_2_minute = int(time_2[0]), int(time_2[1])
-    time_1_military = time_1_hour + 12 if time_1[2] == 'PM' and time_1_hour != 12 else time_1_hour
-    time_2_military = time_2_hour + 12 if time_2[2] == 'PM' and time_2_hour != 12 else time_2_hour
+    time_1, time_2 = matches
 
-    # Convert to string
-    return f"{time_1_military:02d}:{time_1_minute:02d} to {time_2_military:02d}:{time_2_minute:02d}"
+    time_1 = datetime.strptime(time_1[0] + ':' + time_1[1] + ' ' + time_1[2], '%I:%M %p')
+    time_2 = datetime.strptime(time_2[0] + ':' + time_2[1] + ' ' + time_2[2], '%I:%M %p')
+
+    if time_2 < time_1:
+        time_2 += timedelta(days=1)
+
+    time_diff = time_2 - time_1
+    return time_1.strftime('%H:%M') + ' to ' + (time_1 + time_diff).strftime('%H:%M')
