3c2a4954a4a2b24442b6b854f9a48d47644de39f
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 15:59:20 +0000

    Tue, May 2, 2023, 6:59 PM +03:00

--- watch/watch.py
+++ watch/watch.py
@@ -2,36 +2,29 @@ import sys
 import re
 
 def main():
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python watch.py <url>")
+    html = input("HTML: ")
+    url = parse(html)
+    if url:
+        print(url)
+    else:
+        print("None")
 
-    url = sys.argv[1]
-    res = extract_youtube_url(url)
+import re
 
-    if res is not None:
-        print(res)
 
-def extract_youtube_url(url):
-    pattern = r'<iframe.+?src="(.+?)"'
-    match = re.search(pattern, url)
+def parse(s):
+    # Define a regular expression pattern to match a YouTube URL in an iframe src attribute
+    pattern = r'<iframe[^>]*src="https?://(?:www\.)?youtube\.com/embed/([^\s"]+)"[^>]*>'
 
-    if match is not None:
-        iframe_src = match.group(1)
-        pattern = r'youtube\..+?/embed/(.+?)(\?|")'
-        match = re.search(pattern, iframe_src)
+    # Search for the pattern in the input string
+    match = re.search(pattern, s)
 
-        if match is not None:
-            video_id = match.group(1)
-            return f"https://youtube.com/watch?v={video_id}"
+    # If a match is found, return the corresponding youtu.be URL
+    if match:
+        return f"https://youtu.be/{match.group(1)}"
+    else:
+        return None
 
-        pattern = r'youtube\.com/watch\?v=(.+?)(\&|")'
-        match = re.search(pattern, iframe_src)
-
-        if match is not None:
-            video_id = match.group(1)
-            return f"https://youtube.com/watch?v={video_id}"
-
-    return None
 
 if __name__ == "__main__":
     main()
