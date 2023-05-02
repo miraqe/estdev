7b620e6e3439bdb53bbce31c43c91fe612a8d3df
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 16:38:46 +0000

    Tue, May 2, 2023, 7:38 PM +03:00

--- working/working.py
+++ working/working.py
@@ -1,36 +1,30 @@
-from datetime import datetime
 import re
 
 def convert(time_range: str) -> str:
     pattern = re.compile(r'(\d{1,2}):(\d{2})\s*([AP]M)')
-    time_1, time_2 = pattern.findall(time_range)
-    time_1_hour = int(time_1[0])
-    time_1_minute = int(time_1[1])
-    time_1_suffix = time_1[2]
-    time_2_hour = int(time_2[0])
-    time_2_minute = int(time_2[1])
-    time_2_suffix = time_2[2]
-    print(time_1, time_2)
+    times = pattern.findall(time_range)
+    if not times or len(times) != 2:
+        return "Invalid input format"
 
-    if time_1_suffix == 'PM' and time_1_hour != 12:
-        time_1_hour += 12
-    elif time_1_suffix == 'AM' and time_1_hour == 12:
-        time_1_hour = 0
+    time_1, time_2 = times[0], times[1]
+    hour_1, minute_1, period_1 = time_1
+    hour_2, minute_2, period_2 = time_2
 
-    if time_2_suffix == 'PM' and time_2_hour != 12:
-        time_2_hour += 12
-    elif time_2_suffix == 'AM' and time_2_hour == 12:
-        time_2_hour = 0
+    # Convert to 24-hour format
+    if period_1 == 'PM' and hour_1 != '12':
+        hour_1 = str(int(hour_1) + 12)
+    if period_2 == 'PM' and hour_2 != '12':
+        hour_2 = str(int(hour_2) + 12)
+    if period_1 == 'AM' and hour_1 == '12':
+        hour_1 = '00'
+    if period_2 == 'AM' and hour_2 == '12':
+        hour_2 = '00'
 
-    time_1_in_minutes = time_1_hour * 60 + time_1_minute
-    time_2_in_minutes = time_2_hour * 60 + time_2_minute
+    time_1 = f"{hour_1}:{minute_1}"
+    time_2 = f"{hour_2}:{minute_2}"
 
-    if time_2_in_minutes < time_1_in_minutes:
-        time_2_in_minutes += 24 * 60
-
-    time_1_formatted = datetime.strptime(f"{time_1_hour}:{time_1_minute}", "%H:%M")
-    time_2_formatted = datetime.strptime(f"{time_2_hour}:{time_2_minute}", "%H:%M")
-
-    return f"{time_1_formatted.strftime('%H:%M')} to {time_2_formatted.strftime('%H:%M')}"
-
-
+    # Handle ranges that cross over midnight
+    if time_1 < time_2:
+        return f"{time_1} to {time_2}"
+    else:
+        return f"{time_1} to {time_2} + 24 hours"
