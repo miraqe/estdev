03230b5cffe69377c81ebb06273c4517109c080d
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 16:55:08 +0000

    Tue, May 2, 2023, 7:55 PM +03:00

--- working/working.py
+++ working/working.py
@@ -1,5 +1,4 @@
 import re
-from datetime import datetime, timedelta
 
 def convert(time_range: str) -> str:
     pattern = re.compile(r'(\d{1,2}):(\d{2})\s*([AP]M)')
@@ -8,13 +7,28 @@ def convert(time_range: str) -> str:
     if len(matches) != 2:
         raise ValueError("Invalid input format")
 
-    time_1, time_2 = matches
+    time_1 = matches[0]
+    time_2 = matches[1]
 
-    time_1 = datetime.strptime(time_1[0] + ':' + time_1[1] + ' ' + time_1[2], '%I:%M %p')
-    time_2 = datetime.strptime(time_2[0] + ':' + time_2[1] + ' ' + time_2[2], '%I:%M %p')
+    time_1_hour = int(time_1[0])
+    time_1_minute = int(time_1[1])
+    time_1_ampm = time_1[2]
 
-    if time_2 < time_1:
-        time_2 += timedelta(days=1)
+    time_2_hour = int(time_2[0])
+    time_2_minute = int(time_2[1])
+    time_2_ampm = time_2[2]
 
-    time_diff = time_2 - time_1
-    return time_1.strftime('%H:%M') + ' to ' + (time_1 + time_diff).strftime('%H:%M')
+    if time_1_ampm == 'PM' and time_1_hour != 12:
+        time_1_hour += 12
+    elif time_1_ampm == 'AM' and time_1_hour == 12:
+        time_1_hour = 0
+
+    if time_2_ampm == 'PM' and time_2_hour != 12:
+        time_2_hour += 12
+    elif time_2_ampm == 'AM' and time_2_hour == 12:
+        time_2_hour = 0
+
+    time_1_str = f"{time_1_hour:02d}:{time_1_minute:02d}"
+    time_2_str = f"{time_2_hour:02d}:{time_2_minute:02d}"
+
+    return f"{time_1_str} to {time_2_str}"
