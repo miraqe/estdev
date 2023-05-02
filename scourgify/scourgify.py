24cf48bbb0e88758e3b6f96b27614007ab5d7604
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 12:59:04 +0000

    Tue, May 2, 2023, 3:59 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,7 +4,7 @@ import os.path
 
 # Check for correct number of arguments
 if len(sys.argv) != 3:
-    print("Usage: python scourgify.py input.csv")
+    print("Usage: python scourgify.py before.csv after.csv")
     sys.exit(1)
 
 # Check that input file exists
@@ -13,8 +13,8 @@ if not os.path.isfile(sys.argv[1]):
     sys.exit(1)
 
 # Check if output file exists and overwrite if necessary
-if os.path.isfile("after.csv"):
-    print("Warning: 'after.csv' already exists and will be overwritten.")
+if os.path.isfile(sys.argv[2]):
+    print(f"Warning: '{sys.argv[2]}' already exists and will be overwritten.")
 
 # Open input file and read contents
 with open(sys.argv[1], newline='') as csvfile:
@@ -23,13 +23,14 @@ with open(sys.argv[1], newline='') as csvfile:
 
 # Cleanse data
 clean_data = [[cell.strip().replace('\n', ' ') for cell in row] for row in data]
-
-# Add header row to cleansed data
-clean_data.insert(0, ["first", "last", "house"])
+if len(clean_data[0]) != 3 or clean_data[0][0] != 'first' or clean_data[0][1] != 'last' or clean_data[0][2] != 'house':
+    print("Error: CSV file does not have the specified format.")
+    sys.exit(1)
 
 # Write cleansed data to output file
-with open("after.csv", "w", newline='') as outfile:
+with open(sys.argv[2], "w", newline='') as outfile:
     writer = csv.writer(outfile)
     writer.writerows(clean_data)
 
-print("CSV file has been cleansed and saved as 'after.csv'.")
+print(f"CSV file has been cleansed and saved as '{sys.argv[2]}'.")
+
