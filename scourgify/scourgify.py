84bf31a012ba8229db2c143d3c9fca20b893fd90
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 12:43:41 +0000

    Tue, May 2, 2023, 3:43 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,45 +1,32 @@
 import csv
 import sys
+import os.path
 
-# Check for correct usage
-if len(sys.argv) != 3:
-    print("Usage: python scourgify.py input_file.csv")
+# Check for correct number of arguments
+if len(sys.argv) != 2:
+    print("Usage: python scourgify.py input.csv")
     sys.exit(1)
 
 # Check that input file exists
-try:
-    with open(sys.argv[1], "r") as file:
-        csvreader = csv.reader(file)
-        header = next(csvreader)
-except FileNotFoundError:
-    print(f"{sys.argv[1]} not found.")
+if not os.path.isfile(sys.argv[1]):
+    print(f"Error: {sys.argv[1]} not found.")
     sys.exit(1)
 
-# Create the output file
-with open("after.csv", "w", newline="") as file:
-    csvwriter = csv.writer(file)
-    csvwriter.writerow(header)
+# Check if output file exists and overwrite if necessary
+if os.path.isfile("after.csv"):
+    print("Warning: 'after.csv' already exists and will be overwritten.")
 
-    # Iterate over each row of the input file
-    for row in csvreader:
-        # Clean up the data
-        for i, item in enumerate(row):
-            if isinstance(item, str):
-                row[i] = item.strip().title()
+# Open input file and read contents
+with open(sys.argv[1], newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    data = [row for row in reader]
 
-        # Write the cleaned row to the output file
-        csvwriter.writerow(row)
+# Cleanse data
+clean_data = [[cell.strip().replace('\n', ' ') for cell in row] for row in data]
 
-# Check that output file has correct format
-try:
-    with open("after.csv", "r") as file:
-        csvreader = csv.reader(file)
-        header = next(csvreader)
-        if header != ["First Name", "Last Name", "Email", "Phone"]:
-            print("Output file does not have specified format.")
-            sys.exit(1)
-except FileNotFoundError:
-    print("Output file not found.")
-    sys.exit(1)
+# Write cleansed data to output file
+with open("after.csv", "w", newline='') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerows(clean_data)
 
-print("CSV file cleaned successfully!")
+print("CSV file has been cleansed and saved as 'after.csv'.")
