5e35310500188ed627de1e7cc7088c69b84c125b
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-02 14:39:14 +0000

    Tue, May 2, 2023, 5:39 PM +03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,1 +1,9 @@
-code test_numb3rs
+import re
+
+def validate(ip):
+    # Use regex to check that the input is a valid IPv4 address
+    regex = r'^([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])$'
+    if re.search(regex, ip):
+        return True
+    else:
+        return False
