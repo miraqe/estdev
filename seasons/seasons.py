5bd50390aed8452f7d9b771684b6d69c3011ed9c
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 15:39:43 +0000

    Wed, May 3, 2023, 6:39 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,40 +1,20 @@
 from datetime import date, datetime
-import inflect
+
 import sys
-import re
-
-p = inflect.engine()
-
-class Time:
-    def __init__(self, target_date):
-        self.target_date  = target_date
-
-    def __str__(self):
-        # prints out the output at the end
-
-    # operator overload
-    def __sub__(self, other):
-        # perform the subtraction and return the result
-
-    @staticmethod
-    def validate_date(target_date):
-        # use regex to validate the proper format
-        # return boolean (true if the match is found, false otherwise)
-
-    @classmethod
-    def get(cls):
-        # prompt the user for input
-        # validate the input by calling the static method validate_date and pass the user's input
-        # if valid, pass in that user input the cls, otherwise exit using sys.exit() with an error message
-        # Remember datetime.strptime allows you to turn a string into a DateTime object to make it easier for math operations
 
 def main():
-    then = Time.get()
+    try:
+        print(convert_date(get_birthday()))
+    except ValueError:
+        sys.exit(1)
 
-    # gets the current date so no need to ask the user
-    now = Time(date.today())
-    print(now - then)
+def check_birthday():
+    return date.fromisoformat(input("Date of Birth?"))
+
+def convert_date(birthday):
+    time_passed = date.today() - birthday
+    delta = int(timedelta.total_seconds(time_passed) / 60)
+    return(f"{p.number_to_words(delta, andword='')} minutes").capitalize()
 
 if _name_ == "__main__":
     main()
-
