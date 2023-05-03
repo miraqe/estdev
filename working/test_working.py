bc10542f8d71f912d16dd0b4e6ebca0b54a434b9
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 07:45:16 +0000

    Wed, May 3, 2023, 10:45 AM +03:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,35 +1,44 @@
 
-import re
-
-def exists():
-    """working.py and test_working.py exist"""
-    check50.exists("working.py")
-    check50.exists("test_working.py")
 
 
-def imports():
-    """working.py imports libraries other than sys and re"""
-    with open("working.py") as f:
-        if re.search(r"import\s+[^s]", f.read()):
-            raise check50.Failure("working.py should not import any libraries other than sys and re")
+    def test_valid_input_1(self):
+        self.assertEqual(convert("9 AM to 5 PM"), "09:00 to 17:00")
 
+    def test_valid_input_2(self):
+        self.assertEqual(convert("9:00 AM to 5:00 PM"), "09:00 to 17:00")
 
-def converts_9am_to_5pm():
-    """working.py converts "9 AM to 5 PM" to "09:00 to 17:00" """
-    check_converted_time("9 AM to 5 PM", "09:00 to 17:00")
+    def test_valid_input_3(self):
+        self.assertEqual(convert("8 PM to 8 AM"), "20:00 to 08:00")
 
+    def test_valid_input_4(self):
+        self.assertEqual(convert("8:00 PM to 8:00 AM"), "20:00 to 08:00")
 
-def converts_9am_to_5pm_with_zeroes():
-    """working.py converts "9:00 AM to 5:00 PM" to "09:00 to 17:00" """
-    check_converted_time("9:00 AM to 5:00 PM", "09:00 to 17:00")
+    def test_valid_input_5(self):
+        self.assertEqual(convert("12 AM to 12 PM"), "00:00 to 12:00")
 
-def converts_8pm_to_8am():
-    """working.py converts "8 PM to 8 AM" to "20:00 to 08:00" """
-    check_converted_time("8 PM to 8 AM", "20:00 to 08:00")
+    def test_valid_input_6(self):
+        self.assertEqual(convert("12:00 AM to 12:00 PM"), "00:00 to 12:00")
 
-def converts_8pm_to_8am_with_zeroes():
-    """working.py converts "8:00 PM to 8:00 AM" to "20:00 to 08:00" """
-    check_converted_time("8:00 PM to 8:00 AM", "20:00 to 08:00")
+    def test_invalid_input_1(self):
+        with self.assertRaises(ValueError):
+            convert("8:60 AM to 4:60 PM")
 
-def converts_12am_to_12pm():
-    """working.py converts "12 AM to 12 PM" to "
+    def test_invalid_input_2(self):
+        with self.assertRaises(ValueError):
+            convert("9AM to 5PM")
+
+    def test_invalid_input_3(self):
+        with self.assertRaises(ValueError):
+            convert("09:00 to 17:00")
+
+    def test_invalid_input_4(self):
+        with self.assertRaises(ValueError):
+            convert("9 AM - 5 PM")
+
+    def test_invalid_input_5(self):
+        with self.assertRaises(ValueError):
+            convert("10:7 AM - 5:1 PM")
+
+    def test_printing_hours_off_by_one(self):
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            convert
