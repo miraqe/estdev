a2baf16e8e3495b72ac25d880a327bb5f173f70e
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 09:49:31 +0000

    Wed, May 3, 2023, 12:49 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -7,18 +7,7 @@ def test_birthday_extraction():
     assert seasons.extract_birthday("1995-7-3") is None
     assert seasons.extract_birthday("July 3, 1996") is None
 
-def test_minutes_since_birth():
-    assert seasons.minutes_since_birth("2022-05-03") == "Zero minutes"
-    assert seasons.minutes_since_birth("2021-05-03") == "Five hundred twenty-five thousand, six hundred minutes"
-    assert seasons.minutes_since_birth("2020-05-03") == "One million, fifty-one thousand, two hundred minutes"
-    assert seasons.minutes_since_birth("2000-02-29") == "Eleven million, five hundred fifty-eight thousand, four hundred minutes"
-    assert seasons.minutes_since_birth("1999-02-28") == "Eleven million, six hundred twenty-eight thousand minutes"
-    assert seasons.minutes_since_birth("1998-12-31") == "Eleven million, seven hundred six thousand, two hundred forty minutes"
-    assert seasons.minutes_since_birth("1999-01-01") == "Five hundred twenty-five thousand, six hundred minutes"
-    assert seasons.minutes_since_birth("2001-01-01") == "One million, fifty-one thousand, two hundred minutes"
-    assert seasons.minutes_since_birth("1995-01-01") == "Two million, six hundred twenty-nine thousand, four hundred forty minutes"
-    assert seasons.minutes_since_birth("2020-06-01") == "Six million, ninety-two thousand, six hundred forty minutes"
-    assert seasons.minutes_since_birth("1998-06-20") == "Eight hundred six thousand, four hundred minutes"
+
 
 def test_invalid_date_format():
     try:
