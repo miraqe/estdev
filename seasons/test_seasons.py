67bfeb0ba0706cc4d02daa991af0d2fe64a6dfda
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 09:43:50 +0000

    Wed, May 3, 2023, 12:43 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,20 +1,14 @@
 import seasons
 
 def main():
+    birthday()
+
+    assert main("1999-02-28") == ("1999", "02", "28")
+    assert main("1998-12-31") == ("1998", "12", "31")
+    assert main("1999-01-01") == ("1999", "01", "01")
+    assert main("1995-7-3") == None
+    assert main("July 3, 1996") == None
 
 
-    assert sub_date("1999-02-28") == ())
-    assert sub_date("1998-12-31") == "Eleven million, seven hundred six thousand, two hundred forty minutes"
-    assert sub_date("1999-01-01") == "Five hundred twenty-five thousand, six hundred minutes"
-    assert sub_date("1995-01-01") == "Two million, six hundred twenty-nine thousand, four hundred forty minutes"
-    assert sub_date("1998-06-20") == "Eight hundred six thousand, four hundred minutes"
-
-
-def test_invalid_date_format():
-    try:
-        seasons.sub_date("February 6th, 1998")
-    except SystemExit as e:
-        assert e.code == "Invalid date format. Please enter in YYYY-MM-DD format."
-
-def test_exit_code():
-    assert seasons.run() == 0
+if __name__=="__main__":
+    main()
