36745a27e49fbe017c782ec6397b9b08a86e5d47
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 08:34:24 +0000

    Wed, May 3, 2023, 11:34 AM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,14 +1,32 @@
-from datetime import date, timedelta
-from seasons import get_birthdate, minutes_since_birth, main
+import season
 
+def test(season, date, minutes):
+    result = subprocess.run(
+        [sys.executable, "seasons.py", date], stdout=subprocess.PIPE)
+    output = result.stdout.decode().strip()
+    if output != minutes:
+        return f"{season}: expected {minutes}, but got {output}"
+    return None
 
+# set TODAY to January 1, 2000
+TODAY = "2000-01-01"
 
-def test_minutes_since_birth():
-    birthdate = date(2000, 1, 1)
-    now = date.today()
-    expected_minutes = ((now - birthdate).days * 24 * 60) + (now - birthdate).seconds // 60
-    assert minutes_since_birth(birthdate) == expected_minutes
+# run tests
+tests = [
+    ["winter", "1999-01-01", "Five hundred twenty-five thousand, six hundred minutes"],
+    ["spring", "2001-01-01", "One million, fifty-one thousand, two hundred minutes"],
+    ["summer", "1995-01-01", "Two million, six hundred twenty-nine thousand, four hundred forty minutes"],
+    ["fall", "2020-06-01", "Six million, ninety-two thousand, six hundred forty minutes"],
+    ["none", "1998-06-20", "Eight hundred six thousand, four hundred minutes"]
+]
 
+failures = []
+for season, date, minutes in tests:
+    error = test(season, date, minutes)
+    if error:
+        failures.append(error)
 
-def test_main(capsys):
-    birthdate
+if failures:
+    print("\n".join(failures))
+else:
+    print("All checks pass.")
