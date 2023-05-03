25ee5594f0b9078d5bcc14cbb4107210c1f8e583
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 09:38:54 +0000

    Wed, May 3, 2023, 12:38 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,7 +2,8 @@ import seasons
 
 def main():
 
-    assert sub_date("1999-02-28") == "Eleven million, six hundred twenty-eight thousand minutes"
+
+    assert sub_date("1999-02-28") == ())
     assert sub_date("1998-12-31") == "Eleven million, seven hundred six thousand, two hundred forty minutes"
     assert sub_date("1999-01-01") == "Five hundred twenty-five thousand, six hundred minutes"
     assert sub_date("1995-01-01") == "Two million, six hundred twenty-nine thousand, four hundred forty minutes"
