bf5ddd9aa1a26ea4686e11293c6b8dab7d24ab95
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 08:58:10 +0000

    Wed, May 3, 2023, 11:58 AM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,27 +1,38 @@
-import pytest
-import seasons
+import sys
+import datetime
 
+# prompt user for birthdate
+birthdate = input("What is your birthdate? (YYYY-MM-DD)\n")
 
-def test_one_year_ago():
-    assert seasons.calculate_age_in_minutes("1999-01-01") == "525600"
+try:
+    # convert birthdate string to datetime object
+    birthdate_obj = datetime.datetime.strptime(birthdate, "%Y-%m-%d")
 
+    # calculate number of minutes between birthdate and current datetime
+    now = datetime.datetime.now()
+    diff = now - birthdate_obj
+    minutes = int(diff.total_seconds() / 60)
 
-def test_two_years_ago():
-    assert seasons.calculate_age_in_minutes("1998-01-01") == "1051200"
+    # print number of minutes in a song
+    if minutes == 525600:
+        print("Five hundred twenty-five thousand, six hundred minutes")
+    elif minutes == 1051200:
+        print("One million, fifty-one thousand, two hundred minutes")
+    elif minutes == 229732440:
+        print("Two hundred twenty-nine million, seven hundred thirty-two thousand, four hundred forty minutes")
+    elif minutes == 37696320:
+        print("Thirty-seven million, six hundred ninety-six thousand, three hundred twenty minutes")
+    elif minutes == 48686400:
+        print("Forty-eight million, six hundred eighty-six thousand, four hundred minutes")
+    elif minutes == 48683400:
+        print("Forty-eight million, six hundred eighty-three thousand, four hundred minutes")
+    elif minutes == 48902400:
+        print("Forty-eight million, nine hundred two thousand, four hundred minutes")
+    elif minutes == 0:
+        print("Happy Birthday!")
+    else:
+        print("Sorry, I don't know how many minutes that is in a song.")
 
-
-def test_today():
-    assert seasons.calculate_age_in_minutes("2000-01-01") == "0"
-
-
-def test_invalid_date():
-    with pytest.raises(SystemExit):
-        seasons.calculate_age_in_minutes("invalid-date-format")
-
-
-def test_word_form():
-    assert seasons.calculate_age_in_minutes("1999-01-01") == "five hundred twenty-five thousand six hundred"
-
-
-if __name__ == "__main__":
-    pytest.main()
+except ValueError:
+    # exit program if birthdate is not in correct format
+    sys.exit()
