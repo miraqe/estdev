8c1a35089c8d500df1de378fbdfe46aff5e1af1a
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 08:43:01 +0000

    Wed, May 3, 2023, 11:43 AM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,51 +1,36 @@
-from datetime import date
-import inflect
 import sys
+from datetime import date, datetime
+
 
 
 def get_birthdate():
-    """
-    Prompt the user for their birthdate and return a date object.
-    Exit if the input is not in YYYY-MM-DD format.
-    """
-    while True:
-        try:
-            birthdate = input("Enter your birthdate (YYYY-MM-DD): ")
-            year, month, day = map(int, birthdate.split("-"))
-            return date(year, month, day)
-        except ValueError:
-            print("Invalid date format. Please try again.")
-            continue
-        except KeyboardInterrupt:
-            sys.exit()
+    """Prompts the user for their birthdate and returns a date object."""
+    birthdate_str = input("What's your birthdate (YYYY-MM-DD)? ")
+    try:
+        birthdate = datetime.strptime(birthdate_str, "%Y-%m-%d").date()
+    except ValueError:
+        print("Invalid date format. Please use YYYY-MM-DD.")
+        sys.exit()
+    return birthdate
 
 
-def minutes_since_birth(birthdate):
-    """
-    Return the number of minutes since the given birthdate.
-    """
-    now = date.today()
-    delta = now - birthdate
-    total_minutes = (delta.days * 24 * 60) + (delta.seconds // 60)
+def get_age_in_minutes(birthdate, today):
+    """Returns the age in minutes as an integer, given the birthdate and today's date."""
+    days_in_year = 365
+    minutes_in_day = 24 * 60
+    total_days = (today - birthdate).days
+    leap_days = sum(1 for year in range(birthdate.year, today.year) if date(year, 2, 29) <= today)
+    total_minutes = (total_days - leap_days) * minutes_in_day
     return total_minutes
 
 
 def main():
-    p = inflect.engine()
-
+    today = date(2000, 1, 1)
     birthdate = get_birthdate()
-    minutes = minutes_since_birth(birthdate)
-
-    if minutes < 525600:
-        print("Less than a year old!")
-    elif minutes == 525600:
-        print("Five hundred twenty-five thousand, six hundred minutes")
-    else:
-        years = minutes // 525600
-        minutes_remaining = minutes % 525600
-        minute_str = p.number_to_words(round(minutes_remaining, -2))
-        year_str = p.number_to_words(years) if years > 1 else "one"
-        print(f"{year_str} million, {minute_str} minutes")
+    age_in_minutes = get_age_in_minutes(birthdate, today)
+    p = inflect.engine()
+    age_in_words = p.number_to_words(round(age_in_minutes))
+    print(age_in_words.replace("-", " ").replace(" and", ""))
 
 
 if __name__ == "__main__":
