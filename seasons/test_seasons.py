77eeb9dc973b6a7d9e1f6f9e0915a1d1ec0e499a
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 16:00:38 +0000

    Wed, May 3, 2023, 7:00 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,13 +1,13 @@
 from datetime import datetime
-from seasons import check_birthday
+from seasons import minutes_since_birth
 
-def test_check_birthday():
-    assert check_birthday(datetime(1999, 1, 1)) == "Five hundred twenty-five thousand, six hundred minutes"
-    assert check_birthday(datetime(2001, 1, 1)) == "One million, fifty-one thousand, two hundred minutes"
-    assert check_birthday(datetime(1995, 1, 1)) == "Two million, six hundred twenty-nine thousand, four hundred forty minutes"
+def test_minutes_since_birth():
+    assert minutes_since_birth(datetime(1999, 1, 1)) == "Five hundred twenty-five thousand, six hundred minutes"
+    assert minutes_since_birth(datetime(2001, 1, 1)) == "One million, fifty-one thousand, two hundred minutes"
+    assert cminutes_since_birth(datetime(1995, 1, 1)) == "Two million, six hundred twenty-nine thousand, four hundred forty minutes"
 
-    assert check_birthday(datetime(1998, 6, 20)) == "Eight hundred six thousand, four hundred minutes"
-    assert check_birthday("February 6th, 1998") == None
+    assert minutes_since_birth(datetime(1998, 6, 20)) == "Eight hundred six thousand, four hundred minutes"
+    assert minutes_since_birth("February 6th, 1998") == None
 
 if __name__ == "__main__":
-    test_check_birthday()
+    test_minutes_since_birth()
