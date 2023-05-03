f0cd0d322a773c53b0690da56c79de7fe0d73fae
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 08:10:52 +0000

    Wed, May 3, 2023, 11:10 AM +03:00

---
+++ seasons/seasons.py
@@ -0,0 +1,52 @@
+from datetime import date
+import inflect
+import sys
+
+
+def get_birthdate():
+    """
+    Prompt the user for their birthdate and return a date object.
+    Exit if the input is not in YYYY-MM-DD format.
+    """
+    while True:
+        try:
+            birthdate = input("Date of Birth: ")
+            year, month, day = map(int, birthdate.split("-"))
+            return date(year, month, day)
+        except ValueError:
+            print("Invalid date")
+            continue
+        except KeyboardInterrupt:
+            sys.exit()
+
+
+def minutes_since_birth(birthdate):
+    """
+    Return the number of minutes since the given birthdate.
+    """
+    now = date.today()
+    delta = now - birthdate
+    total_minutes = (delta.days * 24 * 60) + (delta.seconds // 60)
+    return total_minutes
+
+
+def main():
+    p = inflect.engine()
+
+    birthdate = get_birthdate()
+    minutes = minutes_since_birth(birthdate)
+
+    if minutes < 525600:
+        print("Less than a year old!")
+    elif minutes == 525600:
+        print("Five hundred twenty-five thousand, six hundred minutes")
+    else:
+        years = minutes // 525600
+        minutes_remaining = minutes % 525600
+        minute_str = p.number_to_words(round(minutes_remaining, -2))
+        year_str = p.number_to_words(years) if years > 1 else "one"
+        print(f"{year_str} million, {minute_str} minutes")
+
+
+if __name__ == "__main__":
+    main()
