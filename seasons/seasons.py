0f428d72a450022483f7d04e3cce6d3e0ac0f020
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 15:29:59 +0000

    Wed, May 3, 2023, 6:29 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,9 +8,9 @@ def main():
     except:
         raise sys.exit('Invalid date')
     p = inflect.engine()
-
+    today = datetime.strptime("2000-01-01", "%Y-%m-%d")
     check_birthday(birth_date)
-
+    time_lapse = today - birth_date
     minutes = time_lapse / timedelta(minutes=1)
     minutes = int(minutes)
     output = p.number_to_words(minutes, andword="")
@@ -21,7 +21,7 @@ def check_birthday(birth_date):
     try:
         birth_date = birth_date
     except:
-        raise sys.exit(ValueError)
+        raise sys.exit('Invalid date')
     return True
 
 if __name__ == "__main__":
