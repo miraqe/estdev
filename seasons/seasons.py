c5b22a918f6cbfd301ff6334dff159505d32c43e
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 15:33:46 +0000

    Wed, May 3, 2023, 6:33 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,28 +1,14 @@
-from datetime import date, datetime, timedelta
+
+from datetime import datetime
 import inflect
 import sys
 
-def main():
-    try:
-        birth_date = datetime.strptime(input("Date of birth: "), '%Y-%m-%d')
-    except:
-        raise sys.exit('Invalid date')
-    p = inflect.engine()
-    today = datetime.strptime("2000-01-01", "%Y-%m-%d")
-    check_birthday(birth_date)
-    time_lapse = today - birth_date
-    minutes = time_lapse / timedelta(minutes=1)
-    minutes = int(minutes)
-    output = p.number_to_words(minutes, andword="")
-    output = output.capitalize() + " minutes"
-    print(output)
-
 def check_birthday(birth_date):
     try:
-        birth_date = birth_date
-    except:
-        raise sys.exit('Invalid date')
+        datetime.strptime(str(birth_date), '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Invalid date")
+    today = datetime.strptime("2000-01-01", "%Y-%m-%d")
+    if birth_date > today:
+        raise ValueError("Birth date is in the future")
     return True
-
-if __name__ == "__main__":
-    main()
