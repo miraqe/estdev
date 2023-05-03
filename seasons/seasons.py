076ae10ac7514aacf39d46e844a078303b16ba76
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 16:08:15 +0000

    Wed, May 3, 2023, 7:08 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,51 +1,33 @@
-import sys
-from datetime import date, datetime
+from datetime import date, timedelta
 import inflect
+import sys
 
-def calculate_age_in_minutes(birthdate):
-    today = date.today()
-    age_in_years = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    leap_years = count_leap_years(birthdate.year, today.year)
-    days = (today - birthdate).days
-    total_minutes = (age_in_years * 365 * 24 * 60) + (leap_years * 24 * 60) + (days * 24 * 60)
-    return total_minutes
+p = inflect.engine()
 
-def count_leap_years(start_year, end_year):
-    count = 0
-    for year in range(start_year, end_year):
-        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-            count += 1
-    return count
-
-def sing_minutes(minutes):
-    p = inflect.engine()
-    years, minutes = divmod(minutes, 365 * 24 * 60)
-    days, minutes = divmod(minutes, 24 * 60)
-    hours, minutes = divmod(minutes, 60)
-    result = []
-    if years > 0:
-        result.append(p.number_to_words(years) + " year" + ("s" if years > 1 else ""))
-    if days > 0:
-        result.append(p.number_to_words(days) + " day" + ("s" if days > 1 else ""))
-    if hours > 0:
-        result.append(p.number_to_words(hours) + " hour" + ("s" if hours > 1 else ""))
-    if minutes > 0:
-        result.append(p.number_to_words(minutes) + " minute" + ("s" if minutes > 1 else ""))
-    return " ".join(result)
 
 def main():
-    try:
-        birthdate_str = input("Please enter your date of birth in YYYY-MM-DD format: ")
-        birthdate = datetime.strptime(birthdate_str, "%Y-%m-%d").date()
-    except ValueError:
-        print("Invalid date format. Please enter your birthdate in YYYY-MM-DD format.")
-        sys.exit(1)
+    birthday = input("Date of Birth: ")
+    minutes = sub_date(birthday)
+    print(convert_totext(minutes))
 
-    age_in_minutes = calculate_age_in_minutes(birthdate)
-    if age_in_minutes < 0:
-        raise ValueError("Minus %s" % sing_minutes(-age_in_minutes))
-    else:
-        print(sing_minutes(age_in_minutes))
+def sub_date(birth):
+    #take input date
+        try:
+            b = date.fromisoformat(birth)
+        except ValueError:
+            sys.exit("Invalid date type")
+        else:
+            #take current date
+            today = date.today()
+            #substract one from another
+            sub = today - b
+            total = sub.total_seconds() / 60
 
-if __name__ == "__main__":
+        return round(total)
+
+def convert_totext(num):
+    return f"{p.number_to_words(num, andword='')} minutes"
+
+
+if _name_ == "__main__":
     main()
