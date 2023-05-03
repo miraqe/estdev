8279ba7d8fd0470afa90fba5dd91201ccc227141
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-03 15:22:24 +0000

    Wed, May 3, 2023, 6:22 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -3,9 +3,9 @@ from seasons import check_birthday
 
 def test_check_birthday():
     assert check_birthday(datetime(1999, 1, 1)) == "Five hundred twenty-five thousand, six hundred minutes"
-    assert check_birthday(datetime(2001, 1, 1)) == "One million, fifty-one thousand, two hundred minutes"
+    assert check_birthday(datetime(2001, 1, 1)) == None
     assert check_birthday(datetime(1995, 1, 1)) == "Two million, six hundred twenty-nine thousand, four hundred forty minutes"
-    assert check_birthday(datetime(2020, 6, 1)) == ("2020", "06", "01")
+    assert check_birthday(datetime(2020, 6, 1)) == None
     assert check_birthday(datetime(1998, 6, 20)) == "Eight hundred six thousand, four hundred minutes"
     assert check_birthday("February 6th, 1998") == None
 
