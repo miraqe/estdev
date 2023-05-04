2559f087b0526077e972ac386dabe725c8e22422
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 12:53:08 +0000

    Thu, May 4, 2023, 3:53 PM +03:00

--- project/test_project.py
+++ project/test_project.py
@@ -4,7 +4,8 @@ def test_add_numbers():
     assert project.add_numbers(2, 3) == 5
     assert project.add_numbers(-2, 3) == 5
     assert project.add_numbers(2, -3) == 5
-    assert project.add_numbers(-2, -3) == 1
+    assert project.add_numbers(-2, -3) == -5
+
 
 def test_multiply_numbers():
     assert project.multiply_numbers(2, 3) == 6
@@ -13,7 +14,7 @@ def test_multiply_numbers():
     assert project.multiply_numbers(-2, -3) == 6
 
 def test_reverse_string():
-    assert project.reverse_string("hello world") == "dlrow olleh"
+    assert project.reverse_string("hello world") == "olleh dlrow"
     assert project.reverse_string("") == ""
     assert project.reverse_string("racecar") == "racecar"
     assert project.reverse_string("The quick brown fox") == "xof nworb kciuq ehT"
