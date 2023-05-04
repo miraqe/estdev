bafd2cf5a3601da7962ccde7124c52e6fb6819b4
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 12:31:36 +0000

    Thu, May 4, 2023, 3:31 PM +03:00

--- project/project.py
+++ project/project.py
@@ -1,5 +1,4 @@
-
-
+import random
 import re
 
 def calculate(expr):
@@ -9,16 +8,15 @@ def calculate(expr):
 
     try:
         result = eval(expr)
-        return f'The result of {expr} is {result}'
+        return f'The result of {expr} is {result:.2f}' if isinstance(result, float) else f'The result of {expr} is {result}'
+    except ZeroDivisionError:
+        return 'Cannot divide by zero. Please try again.'
     except:
         return 'Invalid expression. Please try again.'
 
 
-def test_random_number():
-    result = random_number(1, 10)
-    assert result.startswith('Your random number between 1 and 10 is: ')
-    num = int(result.split()[-1])
-    assert num >= 1 and num <= 10
+def random_number(start, end):
+    return f"Your random number between {start} and {end} is: {random.randint(start, end)}"
 
 
 def text_manipulation(text, operation):
@@ -28,8 +26,11 @@ def text_manipulation(text, operation):
         return text.lower()
     elif operation == "reverse":
         return text[::-1]
+    elif operation == "count":
+        return len(text)
     else:
         return "Invalid operation. Please try again."
+
 
 def decypher(ciphertext, key):
     decrypted = ""
@@ -43,6 +44,7 @@ def decypher(ciphertext, key):
             decrypted += char
     return decrypted
 
+
 def main():
     print("Hi there! My name is AnnaBot! I can help you with the following: calculation, random number, text manipulation, decypher, dictionary, spell check, weather. If you wish to leave the AnnaBot, simply type exit! How can I help you today?")
     while True:
@@ -51,16 +53,24 @@ def main():
             expression = input("What calculation would you like to perform?\n")
             print(calculate(expression))
         elif user_input == "random number":
-            start = int(input("Please enter the starting number:\n"))
-            end = int(input("Please enter the ending number:\n"))
+            try:
+                start = int(input("Please enter the starting number:\n"))
+                end = int(input("Please enter the ending number:\n"))
+            except ValueError:
+                print("Invalid input. Please enter integers only.")
+                continue
             print(random_number(start, end))
         elif user_input == "text manipulation":
             text = input("Please enter some text:\n")
-            operation = input("What operation would you like to perform? (uppercase, lowercase, reverse)\n")
+            operation = input("What operation would you like to perform? (uppercase, lowercase, reverse, count)\n")
             print(text_manipulation(text, operation))
         elif user_input == "decypher":
             ciphertext = input("Please enter the ciphertext:\n")
-            key = int(input("Please enter the key:\n"))
+            try:
+                key = int(input("Please enter the key:\n"))
+            except ValueError:
+                print("Invalid input. Please enter an integer.")
+                continue
             print(decypher(ciphertext, key))
         elif user_input == "dictionary":
             word = input("Please enter a word:\n").lower()
@@ -76,3 +86,7 @@ def main():
             break
         else:
             print("I'm sorry, I didn't understand that. Can you please type your request again? I can help you with calculation, random number, text manipulation, decypher, dictionary, spell check and weather.")
+
+
+if __name__ == "__main__":
+    main
