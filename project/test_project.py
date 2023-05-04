0185e1ecc487a5279e0d8ddb45a166fc60d2d0ac
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 12:19:41 +0000

    Thu, May 4, 2023, 3:19 PM +03:00

--- project/test_project.py
+++ project/test_project.py
@@ -6,15 +6,15 @@ def test_calculate():
     assert project.calculate("10/2") == "The result of 10/2 is 5.00"
     assert project.calculate("4*6") == "The result of 4*6 is 24"
     assert project.calculate("2++2") == "Invalid expression. Please try again."
-    assert project.calculate("2/0") == "Invalid expression. Please try again."
+    assert project.calculate("2/0") == "Cannot divide by zero. Please try again."
     assert project.calculate("abc+def") == "Invalid expression. Please try again."
 
-def test_random_number_generator():
-    result = project.random_number_generator(1, 10)
+def test_random_number():
+    result = project.random_number(1, 10)
     assert result >= 1 and result <= 10
-    result = project.random_number_generator(50, 100)
+    result = project.random_number(50, 100)
     assert result >= 50 and result <= 100
-    result = project.random_number_generator(-10, 0)
+    result = project.random_number(-10, 0)
     assert result >= -10 and result <= 0
 
 def test_text_manipulation():
@@ -25,6 +25,6 @@ def test_text_manipulation():
 
 def test_decypher():
     assert project.decypher("uv vagreangvbany fcnpr fgngvba", 13) == "hi internet security statement"
-    assert project.decypher("d qrfp lbh xabj gung'f jul abj") == "q does you know that's why now"
-    assert project.decypher("lbh zhfg bireqbfr gur vafgvghgr") == "you must overcome the obstacles"
-    assert project.decypher("lbh trg gb gur bgure fvqr!") == "you get to the other side!"
+    assert project.decypher("d qrfp lbh xabj gung'f jul abj", 13) == "q does you know that's why now"
+    assert project.decypher("lbh zhfg bireqbfr gur vafgvghgr", 13) == "you must overcome the obstacles"
+    assert project.decypher("lbh trg gb gur bgure fvqr!", 13) == "you get to the other side!"
