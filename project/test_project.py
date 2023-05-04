837becc8b4194322e6c67a120fd224c9079b2ae3
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 08:47:11 +0000

    Thu, May 4, 2023, 11:47 AM +03:00

--- project/test_project.py
+++ project/test_project.py
@@ -1,5 +1,13 @@
 import project
 
+def calculate(expression):
+    try:
+        result = eval(expression)
+        return f"The result of {expression} is {result}"
+    except:
+        return "Invalid expression. Please try again."
+
+# test_project.py
 def test_calculate():
     assert project.calculate("1+1") == "The result of 1+1 is 2"
     assert project.calculate("2*3") == "The result of 2*3 is 6"
@@ -8,15 +16,3 @@ def test_calculate():
     assert project.calculate("2**3") == "The result of 2**3 is 8"
     assert project.calculate("1.5+2.5") == "The result of 1.5+2.5 is 4.0"
     assert project.calculate("1/0") == "Invalid expression. Please try again."
-
-def test_dictionary():
-    assert project.dictionary("apple") == "Apple: The usually round, red or yellow, edible fruit of a small tree, Malus sylvestris, of the rose family."
-    assert project.dictionary("wordnotfound") == "Word not found. Please try again."
-
-def test_spell_check():
-    assert project.spell_check("Thi is a testt sentence.") == "Error at position 0: Thi (suggested replacements: The)"
-    assert project.spell_check("This is another sentence.") == "No spelling errors found!"
-
-def test_weather():
-    assert project.weather("london") == "The current temperature in London is"
-    assert project.weather("notacity") == "City not found. Please try again."
