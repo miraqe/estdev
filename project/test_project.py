9077911f2ed5a6682d6970b67b047c5ab266886f
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 08:21:29 +0000

    Thu, May 4, 2023, 11:21 AM +03:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,24 +2,12 @@ import project
 
 def test_calculate():
     assert project.calculate("2+2") == 4
-    assert project.calculate("5-3") == 2
-    assert project.calculate("6*7") == 42
-    assert project.calculate("10/2") == 5
-    assert project.calculate("3**3") == 27
-    assert project.calculate("4%3") == 1
-    assert project.calculate("a+b") == None
 
-def test_dictionary_lookup():
-    assert project.dictionary_lookup("apple") == "A fruit with red or yellow or green skin and sweet to tart crisp whitish flesh"
+def test_dictionary():
+    assert project.dictionary("computer") == "a machine for performing calculations automatically"
 
 def test_spell_check():
-    assert project.spell_check("Hello, wrld!") == "Hello, world!"
-    assert project.spell_check("This is a sentence.") == "This is a sentence."
+    assert project.spell_check("Thiss sentence has a spelling mistake.") == "Error at position 1: Thiss (suggested replacements: This, Kiss, Miss, Diss, His)"
 
-def test_temperature_conversion():
-    assert project.temperature_conversion(32, "fahrenheit", "celsius") == 0.0
-    assert project.temperature_conversion(0, "celsius", "fahrenheit") == 32.0
-    assert project.temperature_conversion(100, "celsius", "kelvin") == 373.15
-
-def test_weather_forecast():
-    assert project.weather_forecast("New York") == "The current temperature in New York is XX°C and the weather is cloudy."
+def test_weather():
+    assert project.weather("new york") == "The current temperature in New york is 15°C and the weather is clear sky."
