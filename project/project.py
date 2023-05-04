de4d19e2a8f66567fdab4e8b2ba2a163e7254e14
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 12:51:36 +0000

    Thu, May 4, 2023, 3:51 PM +03:00

--- project/project.py
+++ project/project.py
@@ -1,16 +1,36 @@
 def add_numbers(a, b):
-    return a + b
+    """
+    This function takes two numbers as input (a and b) and returns their sum.
+    If either of the inputs is negative, the function will return the absolute value
+    of their difference. If both inputs are negative, the function will return
+    the negative of their sum.
+    """
+    if a < 0 and b < 0:
+        return -abs(a + b)
+    elif a < 0 or b < 0:
+        return abs(a - b)
+    else:
+        return a + b
 
 def multiply_numbers(a, b):
-    return a * b
+    """
+    This function takes two numbers as input (a and b) and returns their product.
+    If either of the inputs is negative, the function will return the negative
+    of their product. If both inputs are negative, the function will return the
+    positive of their product.
+    """
+    if a < 0 and b < 0:
+        return abs(a) * abs(b)
+    elif a < 0 or b < 0:
+        return -abs(a) * abs(b)
+    else:
+        return a * b
 
 def reverse_string(string):
-    return string[::-1]
-
-def main():
-    print(add_numbers(2, 3))
-    print(multiply_numbers(2, 3))
-    print(reverse_string("hello"))
-
-if __name__ == '__main__':
-    main()
+    """
+    This function takes a string as input and returns the reversed version of the string,
+    with each word reversed but maintaining their position in the string.
+    """
+    words = string.split()
+    reversed_words = [word[::-1] for word in words]
+    return ' '.join(reversed_words)[::-1]
--- project/test_project.py
+++ project/test_project.py
@@ -2,15 +2,18 @@ import project
 
 def test_add_numbers():
     assert project.add_numbers(2, 3) == 5
-    assert project.add_numbers(0, 0) == 0
-    assert project.add_numbers(-1, 1) == 0
+    assert project.add_numbers(-2, 3) == 5
+    assert project.add_numbers(2, -3) == 5
+    assert project.add_numbers(-2, -3) == 1
 
 def test_multiply_numbers():
     assert project.multiply_numbers(2, 3) == 6
-    assert project.multiply_numbers(0, 5) == 0
+    assert project.multiply_numbers(-2, 3) == -6
+    assert project.multiply_numbers(2, -3) == -6
     assert project.multiply_numbers(-2, -3) == 6
 
 def test_reverse_string():
-    assert project.reverse_string("hello") == "olleh"
+    assert project.reverse_string("hello world") == "dlrow olleh"
     assert project.reverse_string("") == ""
     assert project.reverse_string("racecar") == "racecar"
+    assert project.reverse_string("The quick brown fox") == "xof nworb kciuq ehT"
