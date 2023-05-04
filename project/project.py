66f91c8ad203a663d908c7cf9fa72b216b182c1e
miraqe <129060155+miraqe@users.noreply.github.com>
2023-05-04 12:45:47 +0000

    Thu, May 4, 2023, 3:45 PM +03:00

--- project/project.py
+++ project/project.py
@@ -1,80 +1,16 @@
-import random
-import re
+def add_numbers(a, b):
+    return a + b
 
-def calculate(expr):
-    # use regular expressions to check for valid input
-    if not re.match(r'^\d+(\s*[\+\-\*/]\s*\d+)*$', expr):
-        return 'Invalid expression. Please try again.'
+def multiply_numbers(a, b):
+    return a * b
 
-    try:
-        result = eval(expr)
-        return f'The result of {expr} is {result:.2f}' if isinstance(result, float) else f'The result of {expr} is {result}'
-    except ZeroDivisionError:
-        return 'Cannot divide by zero. Please try again.'
-    except:
-        return 'Invalid expression. Please try again.'
-
-
-def random_number(start, end):
-    return f"Your random number between {start} and {end} is: {random.randint(start, end)}"
-
-
-def text_manipulation(text, operation):
-    if operation == "uppercase":
-        return text.upper()
-    elif operation == "lowercase":
-        return text.lower()
-    elif operation == "reverse":
-        return text[::-1]
-    elif operation == "count":
-        return len(text)
-    else:
-        return "Invalid operation. Please try again."
-
-
-def decypher(ciphertext, key):
-    decrypted = ""
-    for char in ciphertext:
-        if char.isalpha():
-            shifted = ord(char) - key
-            if shifted < ord('a'):
-                shifted += 26
-            decrypted += chr(shifted)
-        else:
-            decrypted += char
-    return decrypted
-
+def reverse_string(string):
+    return string[::-1]
 
 def main():
-    print("Hi there! My name is AnnaBot! I can help you with the following: calculation, random number, text manipulation, decypher. If you wish to leave the AnnaBot, simply type exit! How can I help you today?")
-    while True:
-        user_input = input().lower()
-        if user_input == "calculation":
-            expression = input("What calculation would you like to perform?\n")
-            print(calculate(expression))
-        elif user_input == "random number":
-            try:
-                start = int(input("Please enter the starting number:\n"))
-                end = int(input("Please enter the ending number:\n"))
-            except ValueError:
-                print("Invalid input. Please enter integers only.")
-                continue
-            print(random_number(start, end))
-        elif user_input == "text manipulation":
-            text = input("Please enter some text:\n")
-            operation = input("What operation would you like to perform? (uppercase, lowercase, reverse, count)\n")
-            print(text_manipulation(text, operation))
-        elif user_input == "decypher":
-            ciphertext = input("Please enter the ciphertext:\n")
-            try:
-                key = int(input("Please enter the key:\n"))
-            except ValueError:
-                print("Invalid input. Please enter an integer.")
-                continue
-            print(decypher(ciphertext, key))
+    print(add_numbers(2, 3))
+    print(multiply_numbers(2, 3))
+    print(reverse_string("hello"))
 
-        elif user_input == "exit":
-            print("Goodbye!")
-            break
-        else:
-            print("I'm sorry, I didn't understand that. Can you please type your request again? I can help you with calculation, random number, text manipulation, decypher.")
+if __name__ == '__main__':
+    main()
--- project/test_project.py
+++ project/test_project.py
@@ -1,26 +1,16 @@
 import project
 
-def test_calculate():
-    assert project.calculate("2+2") == "The result of 2+2 is 4"
-    assert project.calculate("5-3") == "The result of 5-3 is 2"
-    assert project.calculate("10/2") == "The result of 10/2 is 5.00"
-    assert project.calculate("4*6") == "The result of 4*6 is 24"
-    assert project.calculate("2++2") == "Invalid expression. Please try again."
-    assert project.calculate("2/0") == "Cannot divide by zero. Please try again."
-    assert project.calculate("abc+def") == "Invalid expression. Please try again."
+def test_add_numbers():
+    assert project.add_numbers(2, 3) == 5
+    assert project.add_numbers(0, 0) == 0
+    assert project.add_numbers(-1, 1) == 0
 
-def test_random_number():
-    result = project.random_number(1, 10)
-    assert result >= 1 and result <= 10
+def test_multiply_numbers():
+    assert project.multiply_numbers(2, 3) == 6
+    assert project.multiply_numbers(0, 5) == 0
+    assert project.multiply_numbers(-2, -3) == 6
 
-def test_text_manipulation():
-    assert project.text_manipulation("Hello World", "uppercase") == "HELLO WORLD"
-    assert project.text_manipulation("Hello World", "reverse") == "dlroW olleH"
-    assert project.text_manipulation("Hello World", "count") == 11
-    assert project.text_manipulation("Hello World", "unknown") == "Invalid operation. Please try again."
-
-def test_decypher():
-    assert project.decypher("uv vagreangvbany fcnpr fgngvba", 13) == "hi internet security statement"
-    assert project.decypher("d qrfp lbh xabj gung'f jul abj", 13) == "q does you know that's why now"
-    assert project.decypher("lbh zhfg bireqbfr gur vafgvghgr", 13) == "you must overcome the obstacles"
-    assert project.decypher("lbh trg gb gur bgure fvqr!", 13) == "you get to the other side!"
+def test_reverse_string():
+    assert project.reverse_string("hello") == "olleh"
+    assert project.reverse_string("") == ""
+    assert project.reverse_string("racecar") == "racecar"
